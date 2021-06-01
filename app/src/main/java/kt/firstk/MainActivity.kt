34d5178f675f9bdcba525f7312fa5c806ca34efd
MobDe <MobDevX.outlook.com>
2021-06-01 10:48:23 +0700

    Восстановление с GitHub

--- app/src/main/java/kt/firstk/MainActivity.kt
+++ app/src/main/java/kt/firstk/MainActivity.kt
@@ -2,25 +2,15 @@ package kt.firstk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var editText : EditText
     lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText = findViewById(R.id.editText)
         textView = findViewById(R.id.textView)
-        val year = editText.text.toString().toInt()
-            textView.text = when (year) {
-                2012 -> "Jellybean"
-                2007,2008 -> "До Андроидная эра"
-                in 2015..2019 -> " от Lollipop до Pie "
-            else -> " другие версии "
-        }
     }
 }
